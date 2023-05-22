48917620ebe2b0d51a69dc379d5a9f8db666e2ff
Roger Esteve <rogeresteve30@gmail.com>
2023-05-23 00:46:54 +0200

    shoud be working..

---
+++ server/go.mod
@@ -0,0 +1,8 @@
+module github.com/esteve11/foursomepong/server
+
+go 1.18
+
+require (
+	github.com/golangci/check v0.0.0-20180506172741-cfe4005ccda2 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+)
---
+++ server/src/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin: func(r* http.Request) bool {return true},
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		log.Println(string(p))
+
+		if err := conn.WriteMessage(messageType, p); err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client connected")
+
+	reader(ws)
+}
+
+func main() {
+	fmt.Println("Hello world")
+	http.HandleFunc("/", wsEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
