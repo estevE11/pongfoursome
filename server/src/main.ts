a21fb4254e28a7a28f9f9cc58556cee2268cba4d
Roger Esteve <rogeresteve30@gmail.com>
2023-05-23 03:04:39 +0200

    did more with deno in 10min than go in 2h

---
+++ server/src/main.ts
@@ -0,0 +1,11 @@
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts";
+
+
+const wss = new WebSocketServer(8080);
+wss.on("connection", function (ws: WebSocketClient) {
+  ws.on("message", function (message: string) {
+    console.log("client message: " + message);
+    ws.send(message);
+  });
+});
+
